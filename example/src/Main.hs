9e8970291cf93d1a54af4fdd8957c16578df88a2
David Simmons-Duffin <dsd@caltech.edu>
2020-05-08 12:04:40 -0700

    Simplify documentation for example

--- example/src/Main.hs
+++ example/src/Main.hs
@@ -5,8 +5,8 @@
 
 -- | This module defines a simple routine 'rootBinarySearch' that
 -- performs a binary search for the n-th root of a number and returns
--- the result. The function is then run in parallel in multiple SLURM
--- jobs, and on multiple nodes within each job. Results are stored in
+-- the result. The function is run in parallel in multiple Slurm jobs,
+-- and on multiple nodes/cores within each job. Results are stored in
 -- a database and also sent back to the master process for printing.
 --
 -- The structure of this example computation is as follows
@@ -23,22 +23,20 @@
 --  | |       ------------           |          |          |
 --  |  --------           |          |          |          |
 --  |          |          |          |          |          |
---  Node 1a    Node 1b    Node 2a    Node 2b    Node 3a    Node 3b    <==== IO monad
---  |          |
---  2 cores    2 cores    ...
+--  Node 1a    Node 1b    Node 2a    Node 2b    Node 3a    Node 3b    <==== Process monad
+--  (2 cores)  (2 cores)  (2 cores)  (2 cores)  (2 cores)  (2 cores)
 --
--- The Master process runs a computation in the Cluster monad, which
--- can call 'remoteEval' to automatically evaluate a function using a
--- SLURM job. Each SLURM job runs a computation in the Job monad,
--- which can call 'remoteEval' to automatically evaluate a function on
--- one of the nodes available to the Job. The Job monad keeps track of
--- how many cores are available on each node. In this computation,
--- each node has 2 cores.
+-- The Master process runs in the Cluster monad, which can call
+-- 'remoteEval' to automatically evaluate a function using a Slurm
+-- job. Each Slurm job runs in the Job monad, which can call
+-- 'remoteEval' to automatically evaluate a function on one of the
+-- nodes available to the job. At the lowest level, computations run
+-- in the Process monad from Control.Distributed.Process.
 --
--- Each job is assigned to compute the n-th root of a list of
--- numbers. 3 jobs are run concurrently, with n=2,3,4. Within each
--- job, the numbers are assigned to different cores on different nodes
--- as they become available.
+-- Each job computes the n-th root of a list of numbers. 3 jobs are
+-- run concurrently, with n=2,3,4. Within each job, the numbers are
+-- assigned to different cores on different nodes as they become
+-- available.
 --
 module Main where
 
@@ -53,21 +51,12 @@ import           Hyperion.Util               (curry3, curry4, uncurry3, uncurry4
 import           Options.Applicative         (Parser, auto, help, long, metavar, option, str)
 import           System.FilePath.Posix       ((</>))
 
--- | An interval of 'Double's giving lower and upper bounds for some
--- quantity
+-- | lower and upper bounds for some quantity
 type Bracket = (Double, Double)
 
--- | Given a function @f :: Double -> Double@ and value @x@, search
--- for a 'Bracket' around @f^{-1}(x)@ via binary search until the
--- 'Bracket' has width @eps@. The function is assumed to be
--- monotonically increasing and the initial 'Bracket' is assumed to
--- have the form @(down, up)@, where @up > down@.
-binarySearchInverse
-  :: (Double -> Double) -- ^ function @f@ to invert
-  -> Double             -- ^ argument @x@ to inverse function
-  -> Bracket            -- ^ an initial Bracket for @f^{-1}(x)@
-  -> Double             -- ^ target accuracy @eps@ for binary search
-  -> Bracket            -- ^ a Bracket around the value @f^{-1}(x)@ with accuracy @eps@
+-- | Given a function f and value x, find a bracket of width eps
+-- around f^{-1}(x) via binary search.
+binarySearchInverse :: (Double -> Double) -> Double -> Bracket -> Double -> Bracket
 binarySearchInverse f x bracket eps = go bracket
   where
     go (down, up) | up - down < eps = (down, up)
@@ -76,113 +65,63 @@ binarySearchInverse f x bracket eps = go bracket
       where
         mid = (down + up) / 2
 
--- | Run a binary search for the inverse of @f(x)=x^n@, printing and
--- returning the result.
+-- | Search for inverse of f(x)=x^n, and print the result.
 rootBinarySearch :: Int -> Double -> Bracket -> Double -> IO Bracket
-rootBinarySearch n x brckt eps = do
-  Log.info "Running binary search at " (x, brckt, eps)
-  let result = binarySearchInverse (^n) x brckt eps
+rootBinarySearch n x bracket eps = do
+  Log.info "Running binary search at " (x, bracket, eps)
+  let result = binarySearchInverse (^n) x bracket eps
   Log.info "Computed bracket ------- " result
   return result
 
--- * Preparing for 'remoteEval'
--- $
--- We would like a job to be able to run 'rootBinarySearch' remotely,
--- so that it can make use of all the nodes available to a job. To do
--- so, we must create a @StaticPtr (RemoteFunction a b)@ that can be
--- used with 'remoteEval'.
-
--- | Create the 'StaticPtr' of a 'RemoteFunction', needed by
--- 'remoteEval'. A 'RemoteFunction' takes one argument and returns one
--- result. Thus, we use 'uncurry4' to turn 'rootBinarySearch' into a
--- function that takes one tuple as an argument.
-rootBinarySearchStatic :: StaticPtr (RemoteFunction (Int, Double, Bracket, Double) Bracket)
-rootBinarySearchStatic = static (remoteFnIO $ uncurry4 rootBinarySearch)
-
--- | Given a @StaticPtr (RemoteFunction a b)@, 'remoteEval' evaluates
--- the function on an argument at a remote location. In the 'Job'
--- monad, 'remoteEval' runs the given function on the best available
--- node in the SLURM job. For example, if the job involves 2 nodes
--- with 2 CPUs each, 'remoteEval' checks how many cores are free on
--- each of the 2 nodes and calls 'remoteEval' on the node with the
--- largest number of free cores. In this case, the task requires 1
--- core. If no nodes have at least 1 free core, 'remoteEval' waits
--- until one becomes available. Tasks are run in
--- first-come-first-serve order.
+-- | Run 'rootBinarySearch' remotely on a node available to the job
+-- (location chosen automatically).
 runRemoteRootBinarySearch :: Int -> Double -> Bracket -> Double -> Job Bracket
 runRemoteRootBinarySearch = curry4 $ remoteEval rootBinarySearchStatic
+  where
+    -- | Create a StaticPtr (RemoteFunction a b) that can be used with
+    -- 'remoteEval'. A 'RemoteFunction' must take one argument, so we
+    -- use 'uncurry4'
+    rootBinarySearchStatic = static (remoteFnIO $ uncurry4 rootBinarySearch)
 
--- * Job computation
--- $
--- We now define the main computation for a SLURM job. This
--- computation will be started on one of the nodes of a SLURM job (the
--- "head node"). Using 'remoteEval', it can make use of all the
--- resources in the job in parallel (including its own node).
-
--- | Perform a binary search for the inverse of @f(x)=x^n@ on a
--- collection of @points@. The points are computed concurrently on all
--- cores available to the SLURM job. In this example, the SLURM job
--- has 2 nodes with 2 cores each. Thus, binary searches will be run
--- simultaneously on 4 points at a time. When one search finishes,
--- another is started immediately until all points are finished.
---
--- The results are recorded to the SQLIte database in a table called
--- @binary_root_search@ via the use of 'memoizeWithMap'. The database
--- is the preferred way of storing and retrieving results.
+-- | The main computation for a Slurm job, which runs on the head node
+-- of the job. It concurrently runs computations on different job
+-- nodes via 'remoteEval'.
 binarySearchJob :: Int -> [Double] -> Double -> Job [Bracket]
 binarySearchJob n points eps = do
   Log.info "Computing inverse to x^n with n" n
   Log.info "Running binary searches at     " points
   Log.info "Stopping searches at precision " eps
+  -- | Concurrently compute 'runSearchMemoized n p' for all p in
+  -- points. New computations are automatically started as cores
+  -- become available.
   result <- mapConcurrently (runSearchMemoized n) points
   Log.info "Computed brackets" (zip points result)
   return result
   where
     runSearch :: Int -> Double -> Job Bracket
     runSearch m x = runRemoteRootBinarySearch m x (0, max 1 x) eps
+    -- | 'runSearchMemoized' automatically stores the result of all
+    -- calls to 'runSearch' in the database.
     runSearchMemoized = curry $ memoizeWithMap kvmap $ uncurry runSearch
     kvmap = KeyValMap "binary_root_search"
 
--- * Preparing for remoteEval
--- $
--- We would like to be able to submit concurrent SLURM jobs, each
--- running 'binarySearchJob' on different inputs. To do so, we again
--- have to create a @StaticPtr (RemoteFunction a b)@ for use with
--- 'remoteEval'.
-
--- | Create a 'StaticPtr' of a 'RemoteFunction' so that
--- 'binarySearchJob' can be run remotely. Again, because a
--- 'RemoteFunction' takes a single argument, we use 'uncurry3' to
--- group arguments into a tuple. Because 'binarySearchJob' is in the
--- 'Job' monad instead of 'IO', we must use 'remoteFnJob' to create
--- the 'RemoteFunction'.
-binarySearchJobStatic :: StaticPtr (RemoteFunction ((Int, [Double], Double), ProgramInfo) [Bracket])
-binarySearchJobStatic = static (remoteFnJob $ uncurry3 binarySearchJob)
-
--- | In the 'Cluster' monad, 'remoteEvalJob' runs the given function
--- in a new SLURM job. A job with the needed resources is
--- automatically submitted via @sbatch@. The function
--- 'binarySearchJob' is run in the job and the reult is automatically
--- sent back over the wire.
+-- | Compute 'binarySearchJob' remotely in a new Slurm job (submitted
+-- automatically).
 runRemoteBinarySearchJob :: Int -> [Double] -> Double -> Cluster [Bracket]
 runRemoteBinarySearchJob = curry3 $ remoteEvalJob binarySearchJobStatic
+  where
+    binarySearchJobStatic = static (remoteFnJob $ uncurry3 binarySearchJob)
 
--- * Cluster computation
--- $
--- We now define the master computation, which can be run on a cluster
--- node, login node, or some other machine that can run sbatch. Using
--- 'remoteEval', it runs remote computations concurrently in different
--- SLURM jobs.
-
--- | Compute the inverses of the functions @x^2, x^3, x^4@ acting on
--- 'nPoints' points between 'xMin' and 'xMax'. The computations are
--- run concurrently in 3 differnt SLURM jobs.
+-- | The master computation, which can be run on a cluster node, login
+-- node, or some other machine with access to sbatch. It concurrently
+-- runs remote computations in different Slurm jobs via 'remoteEval'.
 clusterComputation :: ProgramOptions -> Cluster ()
 clusterComputation ProgramOptions{..} =
-  -- For each submitted job, require 2 nodes with 2 cores each
-  local (setJobType MPIJob{ mpiNodes = 2, mpiNTasksPerNode = 2 }) $ do
+  -- | For each submitted job, require 2 nodes with 2 cores each
+  local (setJobType (MPIJob 2 2)) $ do
   Log.text "Running an example computation"
-  -- Concurrently run binarySearchJob in 3 different SLURM jobs
+  -- | Concurrently run runRemoteBinarySearchJob for n=2,3,4. This
+  -- code will submit 3 different Slurm jobs.
   forConcurrently_ [2,3,4] $ \n -> do
     roots <- runRemoteBinarySearchJob n points eps
     Log.info "Computed roots" (n, zip points roots)
@@ -192,11 +131,11 @@ clusterComputation ProgramOptions{..} =
 
 -- | A datatype for command line arguments
 data ProgramOptions = ProgramOptions
-  { xMin          :: Double   -- ^ Lower end of the interval
-  , xMax          :: Double   -- ^ Upper end of the interval
-  , eps           :: Double   -- ^ Desired precision
-  , nPoints       :: Int      -- ^ Number of points to sample in the interval
-  , baseDirectory :: FilePath -- ^ Base directory for log files, databases, etc.
+  { xMin          :: Double
+  , xMax          :: Double
+  , eps           :: Double
+  , nPoints       :: Int
+  , baseDirectory :: FilePath
   } deriving (Show)
 
 -- | Parser for command line arguments
@@ -204,19 +143,18 @@ programOpts :: Parser ProgramOptions
 programOpts = do
   baseDirectory <-
     option str (long "baseDir" <> metavar "PATH"
-      <> help "The base directory for all the stuff (use an absolute path).")
-  xMin <-
-    option auto (long "xMin" <> metavar "NUM" <> help "Lower end of the interval")
-  xMax <-
-    option auto (long "xMax" <> metavar "NUM" <> help "Upper end of the interval")
-  eps <-
-    option auto (long "eps" <> metavar "NUM" <> help "Desired precision")
-  nPoints <-
-    option auto (long "nPoints" <> metavar "INT"
-      <> help "Number of points to sample in the interval")
+                 <> help "The base directory for writing files (absolute path)")
+  xMin    <- option auto (long "xMin" <> metavar "NUM"
+                           <> help "Lower end of the interval")
+  xMax    <- option auto (long "xMax" <> metavar "NUM"
+                          <> help "Upper end of the interval")
+  eps     <- option auto (long "eps"  <> metavar "NUM"
+                          <> help "Desired precision")
+  nPoints <- option auto (long "nPoints" <> metavar "INT"
+                          <> help "Number of points to sample in the interval")
   pure ProgramOptions{..}
 
--- | Given parsed command line arguments, return a 'HyperionConfig'.
+-- | Compute a 'HyperionConfig' from the comand line arguments.
 mkHyperionConfig :: ProgramOptions -> HyperionConfig
 mkHyperionConfig ProgramOptions{..} =
   let
