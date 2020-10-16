4bf19ee1a13fa5c2f2547307c1a45da1240d2eb1
David Simmons-Duffin <dsd@caltech.edu>
2020-10-15 23:17:37 -0700

    Fixes to comments in ObjectId

--- src/Hyperion/ObjectId.hs
+++ src/Hyperion/ObjectId.hs
@@ -1,39 +1,37 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass    #-}
+{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveAnyClass #-}
 
 module Hyperion.ObjectId where
 
-import           Data.BinaryHash             (hashBase64Safe)
-import Control.Monad.IO.Class (MonadIO)
-import Control.Monad.Catch (MonadCatch)
-import Control.Monad.Reader (MonadReader)
-import Data.Typeable (Typeable)
-import qualified Data.Text as Text
-import qualified Hyperion.Database as DB
-import Data.Binary (Binary)
-import GHC.Generics (Generic)
-import Data.Aeson (FromJSON, ToJSON)
+import           Control.Monad.Catch    (MonadCatch)
+import           Control.Monad.IO.Class (MonadIO)
+import           Control.Monad.Reader   (MonadReader)
+import           Data.Aeson             (FromJSON, ToJSON)
+import           Data.Binary            (Binary)
+import           Data.BinaryHash        (hashBase64Safe)
+import qualified Data.Text              as Text
+import           Data.Typeable          (Typeable)
+import           GHC.Generics           (Generic)
+import qualified Hyperion.Database      as DB
 
 -- | An identifier for an object, useful for building filenames and
 -- database entries.
 newtype ObjectId = ObjectId String
   deriving (Eq, Ord, Generic, Binary, FromJSON, ToJSON)
 
--- | Convert an ObjectId to a String. With the current implementation
--- of 'getObjectId', this string will contain only digits.
+-- | Convert an ObjectId to a String.
 objectIdToString :: ObjectId -> String
 objectIdToString (ObjectId i) = "Object_" ++ i
 
--- | Convert an ObjectId to Text. With the current implementation
--- of 'getObjectId', this string will contain only digits.
+-- | Convert an ObjectId to Text.
 objectIdToText :: ObjectId -> Text.Text
 objectIdToText = Text.pack . objectIdToString
 
--- | The ObjectId of an object is the absolute value of its hash. The
--- first time it is called on a given object, 'getObjectId' comptues
--- the ObjectId and stores it in the database before returning
--- it. Subsequent calls read the value from the database.
+-- | The ObjectId of an object is the result of 'hashBase64Safe'. The
+-- first time 'getObjectId' is called, it comptues the ObjectId and
+-- stores it in the database before returning it. Subsequent calls
+-- read the value from the database.
 getObjectId
   :: ( Binary a
      , Typeable a
